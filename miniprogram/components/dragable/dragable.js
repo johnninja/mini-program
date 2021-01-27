e8976bee6ff1dd7d8badf854874ac8418faadfb1
张旺 <zhangwang@boxfish.cn>
2021-01-27 18:35:23 +0800

    拖拽优化

--- miniprogram/components/dragable/dragable.js
+++ miniprogram/components/dragable/dragable.js
@@ -24,13 +24,9 @@ Component({
       type: Number,
       value: 3
     },
-    leftConnected: {
-      type: Boolean,
-      value: false
-    },
-    topConnected: {
-      type: Boolean,
-      value: false
+    rects: {
+      type: Array,
+      value: []
     }
   },
   observers: {
@@ -59,7 +55,17 @@ Component({
     startY: 0,
     control: null,
     leftConnectedStatus: false,
-    topConnectedStatus: false
+    topConnectedStatus: false,
+    leftConnected: false,
+    topConnected: false,
+    guide: {
+      left: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+      center: 0,
+      middle: 0
+    },
   },
 
   /**
@@ -67,7 +73,7 @@ Component({
    */
   methods: {
     onTouchStart (e) {
-      const { left, top, width, height } = this.data
+      const { guide } = this.data
       const touch = e.touches[0]
       const { clientX, clientY } = touch
       const { control } = e.target.dataset
@@ -78,7 +84,7 @@ Component({
         control: control ? control.split(',').map(item => item.trim()) : null
       })
       wx.nextTick(() => {
-        this.triggerEvent('dragstart', { left, top, width, height })
+        this.triggerEvent('dragstart', { guide })
       })
     },
     onTouchMove (e) {
@@ -94,16 +100,9 @@ Component({
       deltaX = deltaX > 0 ? Math.ceil(deltaX) : Math.floor(deltaX)
       deltaY = deltaY > 0 ? Math.ceil(deltaY) : Math.floor(deltaY)
 
-      if (leftConnected && Math.abs(deltaX) < gap) {
-        deltaX = 0
-      }
-      if (topConnected && Math.abs(deltaY) < gap) {
-        deltaY = 0
-      }
-
       const temp = {
-        left: left + deltaX,
-        top: top + deltaY,
+        left: left + (leftConnected && Math.abs(deltaX) < gap ? 0 : deltaX),
+        top: top + (topConnected && Math.abs(deltaY) < gap ? 0 : deltaY),
         width: widthNegtive ? width - deltaX : width + deltaX,
         height: heightNegtive ? height - deltaY : height + deltaY
       }
@@ -122,16 +121,23 @@ Component({
         }
       }
       
+      const { collision, drag } = this.collisionDetect({
+        left, top, width, height,
+        ...targetObj,
+        resize: !!control
+      })
+
       this.setData({
         startX: clientX,
         startY: clientY,
-        ...targetObj
+        guide: collision,
+        left, top, width, height,
+        ...targetObj,
+        ...drag
       })
       wx.nextTick(() => {
         this.triggerEvent('dragging', {
-          left, top, width, height,
-          ...targetObj,
-          resize: !!control
+          guide: collision
         })
       })
     },
@@ -144,9 +150,88 @@ Component({
       wx.nextTick(() => {
         this.triggerEvent('dragend')
       })
+    },
+    collisionDetect (source) {
+      const distance = this.distance.bind(this)
+      const { rects } = this.data
+      let collision = {
+        left: 0,
+        top: 0,
+        bottom: 0,
+        right: 0,
+        center: 0,
+        middle: 0
+      }
+      let drag = {}
+  
+      rects.forEach(col => {
+        const left = source.left
+        const right = source.left + source.width
+        const top = source.top
+        const bottom = source.top + source.height
+        const center = source.left + source.width / 2
+        const middle = source.top + source.height / 2
+  
+        // left
+        if (distance(left, col.left)) {
+          drag.left = collision.left = col.left
+          drag.leftConnected = true
+        }
+        if (distance(left, col.left + col.width)) {
+          drag.left = collision.left = col.left + col.width
+          drag.leftConnected = true
+        }
+        // top
+        if (distance(top, col.top)) {
+          drag.top = collision.top = col.top
+          drag.topConnected = true
+        }
+        if (distance(top, col.top + col.height)) {
+          drag.top = collision.top = col.top + col.height
+          drag.topConnected = true
+        }
+  
+        // right
+        if (distance(right, (col.left + col.width))) {
+          collision.right = col.left + col.width
+          drag.width = collision.right - left
+          drag.leftConnected = true
+        }
+        if (distance(right, col.left)) {
+          collision.right = col.left
+          drag.width = collision.right - left
+          drag.leftConnected = true
+        }
+        // bottom
+        if (distance(bottom, (col.top + col.height))) {
+          collision.bottom = col.top + col.height
+          drag.height = collision.bottom - top
+          drag.topConnected = true
+        }
+        if (distance(bottom, col.top)) {
+          collision.bottom = col.top
+          drag.height = collision.bottom - top
+          drag.topConnected = true
+        }
+  
+        // center
+        if (!source.resize && distance(center, col.left + col.width / 2)) {
+          collision.center = col.left + col.width / 2
+          drag.left = collision.center - source.width / 2
+          drag.leftConnected = true
+        }
+        // middle
+        if (!source.resize && distance(middle, col.top + col.height / 2)) {
+          collision.middle = col.top + col.height / 2
+          drag.top = collision.middle - source.height / 2
+          drag.topConnected = true
+        }
+      })
+  
+      return { collision, drag }
+    },
+    distance(a, b) {
+      return Math.abs(a - b) < this.data.gap
     }
-  },
-  attached () {
-    
   }
 })
--- miniprogram/pages/editor/index.js
+++ miniprogram/pages/editor/index.js
@@ -1,7 +1,5 @@
 // miniprogram/pages/demo/index.js
 const app = getApp()
-let timer = null
-
 Page({
 
   /**
@@ -20,12 +18,11 @@ Page({
       width: 200,
       height: 150,
       gap: 2,
-      leftConnected: false,
-      rightConnected: false
+      rects: []
     },
     grid: {
-      width: 20,
-      span: 4,
+      width: 0,
+      span: 0,
       gutter: 20,
       padding: 20
     },
@@ -41,22 +38,27 @@ Page({
     elements: [],
     showGuide: false
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const drag = this.data.drag
+    const { page, drag, grids, elements } = this.data
     const width = app.globalData.winWidth
     const height = app.globalData.winHeight
+    const pageData = {
+      ...page,
+      width,
+      height
+    }
     this.setData({
-      page: {
-        ...this.data.page,
-        width,
-        height
-      },
+      page: pageData,
       drag: {
         ...drag,
+        rects: [
+          ...grids,
+          ...elements,
+          pageData
+        ],
         left: (width - drag.width) / 2,
         top: (height - drag.height) / 2
       }
@@ -64,13 +66,17 @@ Page({
   },
 
   onDragging (e) {
-    const data = e.detail
-    this.setGuideLine(data)
+    const { guide } = e.detail
+    this.setData({
+      guide
+    })
   },
   onDragStart (e) {
-    const data = e.detail
-    this.setData({showGuide: true})
-    this.setGuideLine(data)
+    const { guide } = e.detail
+    this.setData({
+      showGuide: true,
+      guide
+    })
   },
   onDragEnd () {
     this.setData({
@@ -85,109 +91,16 @@ Page({
       }
     })
   },
-  setGuideLine (data) {
-    const { collision, drag } = this.collisionDetect(data)
-
-    this.setData({
-      drag: {
-        ...this.data.drag,
-        ...data,
-        ...drag,
-        leftConnected: drag.leftConnected || false,
-        topConnected: drag.topConnected || false
-      },
-      guide: {
-        ...collision
-      }
-    })
-  },
   onGridLoaded (e) {
     const data = e.detail
+    const { page, elements, grids } = this.data
     this.setData({
-      grids: data
+      grids: data,
+      'drag.rects': [
+        ...elements,
+        ...grids,
+        page
+      ]
     })
-  },
-  collisionDetect (source) {
-    const distance = this.distance
-    const { elements, grids, page } = this.data
-    let mixEles = [...elements, ...grids, page]
-    let collision = {
-      left: 0,
-      top: 0,
-      bottom: 0,
-      right: 0,
-      center: 0,
-      middle: 0
-    }
-    let drag = {}
-
-    mixEles.forEach(col => {
-      const left = source.left
-      const right = source.left + source.width
-      const top = source.top
-      const bottom = source.top + source.height
-      const center = source.left + source.width / 2
-      const middle = source.top + source.height / 2
-
-      // left
-      if (distance(left, col.left)) {
-        drag.left = collision.left = col.left
-        drag.leftConnected = true
-      }
-      if (distance(left, col.left + col.width)) {
-        drag.left = collision.left = col.left + col.width
-        drag.leftConnected = true
-      }
-      // top
-      if (distance(top, col.top)) {
-        drag.top = collision.top = col.top
-        drag.topConnected = true
-      }
-      if (distance(top, col.top + col.height)) {
-        drag.top = collision.top = col.top + col.height
-        drag.topConnected = true
-      }
-
-      // right
-      if (distance(right, (col.left + col.width))) {
-        collision.right = col.left + col.width
-        drag.width = collision.right - left
-        drag.leftConnected = true
-      }
-      if (distance(right, col.left)) {
-        collision.right = col.left
-        drag.width = collision.right - left
-        drag.leftConnected = true
-      }
-      // bottom
-      if (distance(bottom, (col.top + col.height))) {
-        collision.bottom = col.top + col.height
-        drag.height = collision.bottom - top
-        drag.topConnected = true
-      }
-      if (distance(bottom, col.top)) {
-        collision.bottom = col.top
-        drag.height = collision.bottom - top
-        drag.topConnected = true
-      }
-
-      // center
-      if (!source.resize && distance(center, col.left + col.width / 2)) {
-        collision.center = col.left + col.width / 2
-        drag.left = collision.center - source.width / 2
-        drag.leftConnected = true
-      }
-      // middle
-      if (!source.resize && distance(middle, col.top + col.height / 2)) {
-        collision.middle = col.top + col.height / 2
-        drag.top = collision.middle - source.height / 2
-        drag.topConnected = true
-      }
-    })
-
-    return { collision, drag }
-  },
-  distance(a, b) {
-    return Math.abs(a - b) < this.data.drag.gap
   }
 })
